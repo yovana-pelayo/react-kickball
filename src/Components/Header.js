66325456d89f7c526a0351807bd9b017fb54970a
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-25 09:54:24 -0700

    added home link to header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 export default function Header() {
   return (
-    // <NavLink exact to="/">Home</NavLink>
-    <NavLink exact to="/teams">
-      Teams
-    </NavLink>
+    <div className="Header">
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink exact to="/teams">
+        Teams
+      </NavLink>
+    </div>
   );
 }
